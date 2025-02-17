c79a6397cc4ae4c084d31d5f9a95caf22feb85d3
VIRTUOSGAMESdKrasnikov <dkrasnikov@virtuosgames.com>
2025-02-17 20:56:12 +0200

    added base player input

---
+++ Assets/_Project/Scripts/PlayerController/PlayerMovementController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using BrackeysJam.PlayerController.InputReaders;
+using UnityEngine;
+
+namespace BrackeysJam.PlayerController
+{
+    public class PlayerMovementController : MonoBehaviour, IEnableable
+    {
+        private IInputReader<Vector2> _inputReader = new WASDInputReader();
+        private Vector2 _movementVector;
+        private float _speed;
+        private Rigidbody2D _rb;
+        
+        private bool _enabled = false;
+
+        public void Initialize(float speed, Rigidbody2D rb)
+        {
+            _speed = speed;
+            _rb = rb;
+
+            Enable();
+        }
+        
+        private void Update()
+        {
+            _movementVector = _inputReader.Read() * _speed;
+        }
+
+        private void FixedUpdate()
+        {
+            _rb.AddForce(_movementVector, ForceMode2D.Impulse);
+        }
+
+        #region IEnableable
+
+        public void Enable()
+        {
+            _enabled = true;
+        }
+
+        public void Disable()
+        {
+            _enabled = false;
+        }
+
+        #endregion
+    }
+}
